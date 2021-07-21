19173293ed5b8cd8b3a3ed5c902c5adf18e11f90
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-07-21 21:29:04 +0200

    Windows Update

---
+++ windows-update.ps1
@@ -0,0 +1,10 @@
+﻿Add-Type -assembly System.Windows.Forms
+$popup = New-Object System.Windows.Forms.Form
+$popup.Text ="Mise à jour Windows"
+$popup.Width = 300
+$popup.Height = 150
+#$main_form.AutoSize = $true
+$popup.ShowDialog()
+
+#Get-WindowsUpdate
+#Install-WindowsUpdate -AcceptAll
