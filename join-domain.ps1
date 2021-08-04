d36e4b1636586fdea651b1fdbb7a72ebb08bb224
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-08-04 21:58:28 +0200

    Join a domain

---
+++ join-domain.ps1
@@ -0,0 +1,7 @@
+﻿$domain = "test.com"
+$user = "utilisateur"
+$password = "MDP"
+$Domainuser ="$domain\$user"
+$credential = New-Object System.Management.Automation.PSCredential($Domainuser, $password)
+
+Add-Computer -DomainName $domain -Credential $Domainuser -Restart -Force
